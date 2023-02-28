12eceefb47fdf23df66e1e1c1c15c4d5a7a8b780
Alexander <continenttal2015@yandex.ru>
2023-02-28 13:48:56 +0300

    Completed task 09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, котроая выводит случайное 
+// числоиз отрезка [10, 99] и показывает наибольшую цифру
+
+// 78-> 8
+// 12-> 2
+// 85-> 8
+
+int number=new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+int firstDigit = number / 10;
+int secontDigit = number % 10;
+// if (firstDigit > secontDigit)
+//    {
+//     Console.WriteLine($"Наибольшая цифра {firstDigit}");
+//    }
+//    else
+//    {
+//     Console.WriteLine($"Наибольшая цифра {secontDigit}");
+//    }
+
+   int result = firstDigit > secontDigit ? firstDigit : secontDigit;
+   Console.WriteLine($"Наибольшая цифра {result}");
