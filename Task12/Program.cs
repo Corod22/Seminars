fcb8cae849c81cafac4eb56ef8ad6662b2ccd7a3
Alexander <continenttal2015@yandex.ru>
2023-02-28 15:56:05 +0300

    Completed task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -4,11 +4,25 @@
 
 // 34, 5-> не кратно, остаток 4
 // 16, 4-> кратно
-Console.WriteLine("Введите 1 число");
+// Console.WriteLine("Введите 1 число");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// int multiplicityNumber=firstNumber%secondNumber;
+// if (multiplicityNumber==0)
+// {
+//     Console.WriteLine("Кратно");
+// }
+// else
+// {
+//     Console.WriteLine($"Не кратно, остаток-> {multiplicityNumber}");
+// }
+
+Console.WriteLine("Введите первое число");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
-int multiplicityNumber=firstNumber%secondNumber;
+int multiplicityNumber = MultiplicityNumber(firstNumber, secondNumber);
 if (multiplicityNumber==0)
 {
     Console.WriteLine("Кратно");
@@ -16,4 +30,10 @@ if (multiplicityNumber==0)
 else
 {
     Console.WriteLine($"Не кратно, остаток-> {multiplicityNumber}");
+}
+
+int MultiplicityNumber(int first, int second)
+{
+    int result = first %second;
+    return result;
 }
